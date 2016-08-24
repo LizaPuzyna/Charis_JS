f61f0d8d06f3d75bbad31a1087f686d7f1f30cbb
Elizabeth <elizabethpuzyna01@gmail.com>
2016-08-24 13:58:47 +0300

    Add TackPicture from ini file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,41 @@
-var s = 'RDDRRUURDDDDLLLLURULUU';
+var s = '';
+
+function getXmlHttp() {
+    var xmlhttp;
+    try {
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        try {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (E) {
+            xmlhttp = false;
+        }
+    }
+    if (!xmlhttp && typeof XMLHttpRequest != 'undefined') {
+        xmlhttp = new XMLHttpRequest();
+    }
+    return xmlhttp;
+}
+var response = undefined;
+(function () {
+    var xmlhttp = getXmlHttp();
+    xmlhttp.open('GET', 'task.ini', false);
+    xmlhttp.send(null);
+    if (xmlhttp.status == 200) {
+        response = xmlhttp.responseText;
+        alert(response);
+    }
+})();
+var splitted = response.split("\n", 1000);
+console.log(splitted);
+
+for(var i = 0; i < splitted.length; i++){
+    var spl = splitted[i].split("=", 1000);
+    if (spl[0]=="TaskPicture"){
+        s = spl[1];
+        console.log(s);
+    }
+}
 
 var bw = 200;
 var bh = 200;
@@ -12,7 +49,7 @@ var b = padding;
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-function drawBoard(){
+function drawBoard() {
     for (var x = 0; x <= bw; x += 50) {
         context.moveTo(0.5 + x + padding, padding);
         context.lineTo(0.5 + x + padding, bh + padding);
@@ -26,24 +63,23 @@ function drawBoard(){
 }
 
 function par(s) {
-    for(var i = 0; i < s.length; i++){
+    for (var i = 0; i < s.length; i++) {
         move(s[i]);
     }
-
 }
 
-function move(str){
+function move(str) {
     context.moveTo(a, b);
-    if(str == "R"){
-        a +=50;
+    if (str == "R") {
+        a += 50;
     }
-    else if(str == "D"){
+    else if (str == "D") {
         b += 50;
     }
-    else if(str == "L"){
+    else if (str == "L") {
         a -= 50;
     }
-    else if(str == "U"){
+    else if (str == "U") {
         b -= 50;
     }
     context.lineTo(a, b);
