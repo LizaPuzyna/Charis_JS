9ed16853f07ac7537d789394755a51848f6e7169
Андрей-Сухаревской <andr.sukharev@gmail.com>
2016-08-24 22:42:03 +0200

    fix bugs

--- js/index.js
+++ js/index.js
@@ -1,99 +1,82 @@
-var str = '';
+var str = getParam("TaskPicture");
 
-    var width = 7;
-    var height = 5;
+var width = 7;
+var height = 5;
+var n = 50;
 
-    var bw = width * 50;
-    var bh = height * 50;
-    var padding = 4;
-    var j = 0;
+var bw = width * n;
+var bh = height * n;
+var padding = 4;
+var j = 0;
 
+//var cw = bw + (padding*2) + 1;
+//var ch = bh + (padding*2) + 1;
 
-    var x = padding;
-    var y = padding;
+var x = padding;
+var y = padding;
 
-    var canvas1 = document.getElementById("canvas1");
-    var context1 = canvas1.getContext("2d");
-    var context2 = canvas1.getContext("2d");
-    var canvas2 = document.getElementById("canvas2");
-    var context3 = canvas2.getContext("2d");
-    var context4 = canvas2.getContext("2d");
+var canvas1 = document.getElementById("canvas1");
+var context1 = canvas1.getContext("2d");
+var context2 = canvas1.getContext("2d");
+var canvas2 = document.getElementById("canvas2");
+var context3 = canvas2.getContext("2d");
+var context4 = canvas2.getContext("2d");
 
-
-    function drawBoard(context) {
-        for (var i = 0; i <= bw; i += 50) {
-            context.moveTo(0.5 + i + padding, padding);
-            context.lineTo(0.5 + i + padding, bh + padding);
-        }
-        for (var i = 0; i <= bh; i += 50) {
-            context.moveTo(padding, 0.5 + i + padding);
-            context.lineTo(bw + padding, 0.5 + i + padding);
-        }
-        context.strokeStyle = "#CBDBE1";
-        context.stroke();
+function drawBoard(context) {
+    for (var i = 0; i <= bw; i += n) {
+        context.moveTo(0.5 + i + padding, padding);
+        context.lineTo(0.5 + i + padding, bh + padding);
     }
+    for (var i = 0; i <= bh; i += n) {
+        context.moveTo(padding, 0.5 + i + padding);
+        context.lineTo(bw + padding, 0.5 + i + padding);
+    }
+    context.strokeStyle = "#CBDBE1";
+    context.stroke();
+}
 
+function par(s) {
+    for (var i = 0; i < s.length; i++) {
+        console.log(s[i]);
+        if ((isNaN(s[i + 1]) == false) && (i != s.length - 2)) {
 
-
-    function par(s) {
-        for (var i = 0; i < s.length; i++) {
-            if (isNaN(s[i]) == false) {
-                for (j = 0; j < s[i]; j++) {
-                    move(s[i + 1]);
-                }
-
-                i += 1;
-            }
-            else {
+            for (j = 0; j < s[i + 1]; j++) {
                 move(s[i]);
             }
 
+            i += 1;
+        }
+        else {
+            move(s[i]);
         }
 
     }
 
+}
 
-
-    function move(str) {
-
-        context2.moveTo(x, y);
-        context4.moveTo(x, y);
-        if (str == "R") {
-            x += 50;
-        }
-        else if (str == "D") {
-            y += 50;
-        }
-        else if (str == "L") {
-            x -= 50;
-        }
-        else if (str == "U") {
-            y -= 50;
-        }
-        /*else if (str == "r") {
-            x += 50;
-            y -= 50;
-        }
-        else if (str == "l") {
-            x -= 50;
-            y += 50;
-        }
-        else if (str == "u") {
-            x += 50;
-            y += 50;
-        }
-        else if (str == "d") {
-            x -= 50;
-            y -= 50;
-        }*/
-        context2.lineTo(x, y);
-        context2.stroke();
-        context4.lineTo(x, y);
-        context4.stroke();
-
+function move(str) {
+    context2.moveTo(x, y);
+    context4.moveTo(x, y);
+    if (str == "R") {
+        x += n;
     }
+    else if (str == "D") {
+        y += n;
+    }
+    else if (str == "L") {
+        x -= n;
+    }
+    else if (str == "U") {
+        y -= n;
+    }
+    context2.lineTo(x, y);
+    context2.stroke();
+    context4.lineTo(x, y);
+    context4.stroke();
 
-    drawBoard(context1);
-    drawBoard(context3);
+}
 
-    par(str);
+drawBoard(context1);
+drawBoard(context3);
+
+par(str);
